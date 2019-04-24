4fa7d1c050708f27a3bda5f3190a9a422ce49a40
Kornel Penkala <kornelpen@gmail.com>
2019-04-24 21:58:35 +0200

    Basic search support has been added. Navigation's appearance changed.

--- rental/src/Components/MainPage/home.js
+++ rental/src/Components/MainPage/home.js
@@ -2,8 +2,66 @@ import React, { Component } from 'react';
 import './home.css';
 import logo from '../../fotos/back.jpg'
 import { Button, Navbar, Form,ButtonGroup } from 'react-bootstrap';
+import NumberFormat from 'react-number-format';
 
 class home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            search : "",
+            offerts: []
+        }
+        this.onChange = this.onChange.bind(this);
+        this.searchOffer = this.searchOffer.bind(this);
+    }
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
+        
+      }
+
+      searchOffer(){
+        if(this.state.search !==undefined){
+            fetch('https://localhost:44359/api/Offers/Query/' + this.state.search)
+            .then(response => response.json())
+            .then(parseJSON => {
+                this.setState({
+                    offerts: parseJSON.value || []
+                })
+            })
+        }
+        
+      }
+
+      showOfferts(){
+          if(this.state.offerts.length>0){
+              return(
+                this.state.offerts.map((item, i) => (
+                    <div className="offcon" key={i}>
+                        <div className="offoto"><img src={"https://localhost:44359/" + item.photos[0]} alt="foto" /></div>
+                        <div className="ofdesc">{item.title}<hr />
+                            <div className="ofinf">
+                                <div className="localization"> {item.city}, {item.street}</div>
+                                <ul>
+                                    <li>Powierzchnia : {item.area} m²</li>
+                                    <li>Piętro : {item.level}</li>
+                                    <li>Liczba pokoi : {item.roomCount}</li>
+                                    <li>Kategoria : {item.categoryName}</li>
+                                    <li>Rodzaj ogłoszenia : {item.offerTypeType}</li>
+                                </ul>
+
+                            </div>
+                        </div>
+                        <div className="ofdes">
+                            <div className="ofprice"><NumberFormat value={item.price} displayType={'text'} thousandSeparator={' '} /> zł</div>
+                            <div className="ofbutton"><Button disabled href={"/detailsoff/" + i}>Szczegóły</Button></div>
+                        </div>
+                        <div className="clearfix"></div>
+                    </div>
+
+                ))
+              )
+          }
+      }
     render() {
       return (
           <div className="box">  
@@ -24,18 +82,13 @@ class home extends Component {
                 <div className="description">U nas możesz wynająć nawet budę</div>
                 <div className="subdescription">wyszukaj interesujących cię ofert</div>
                     <div className="searchbox arrow">
-                        <select>
-                            <option value="mieszkanie">Mieszkanie</option>
-                            <option value="dom">Dom</option>
-                        </select>
-                        <select>
-                            <option value="kupno">Kupno</option>
-                            <option value="wynajem">Wynajem</option>
-                        </select>
-                        <input type="text" placeholder="Miejscowość"  />
-                        <button value="wyszukaj">Wyszukaj</button>
+                        <input type="text" placeholder="Wyszukaj" name="search" onChange={this.onChange} />
+                        <button value="wyszukaj" onClick={this.searchOffer}>Wyszukaj</button>
                     </div>
-                    </div>            
+                    </div> 
+                    {
+                        this.showOfferts()
+                    }           
                     <div className="offerts">
                     <p className="random">Przykładowe nasze oferty</p>
                     <div className="offert">
