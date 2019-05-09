4cbaf94baadca3505a63e5a722789453d4eafe01
Gatok <miko4500@gmail.com>
2019-05-09 15:55:22 +0200

    Fixed typo in error

--- Rentall.Commons/ErrorMessages/OfferErrors.cs
+++ Rentall.Commons/ErrorMessages/OfferErrors.cs
@@ -4,7 +4,7 @@
     {
         public static readonly string NotFoundById = "Oferta o podanym id nie istnieje.";
         public static readonly string AddingError = "Nie udało się dodać oferty.";
-        public static readonly string NotFoundOffersByUser = "Użytkownik nie utworzył rzadnych ofert.";
+        public static readonly string NotFoundOffersByUser = "Użytkownik nie utworzył żadnych ofert.";
         public static readonly string NotFoundByQuery = "Nie znaleziono ofert.";
         public static readonly string NotAllowed = "Nie możesz wykonać tej akcji.";
     }
